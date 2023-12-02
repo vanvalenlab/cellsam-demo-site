7e388d9eebc7a88b7558453c216848d1cf4d2508
Rohit Dilip <rdilip1137@gmail.com>
2023-12-01 22:44:22 -0800

    changed visually, added example

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
-import FileUpload from "./Fileupload";
+import FileUpload from "./FileUpload";
 import Header from "./Header";
+import DownloadButton from "./Download";
 
 function App() {
   return (
@@ -8,20 +9,31 @@ function App() {
       <Header />
 
       <main className="container mx-auto p-8">
-        <section className="flex justify-between items-center mb-8">
+        <section className="mb-12 text-center">
           <h1 className="text-5xl font-bold text-gray-800">CellSAM</h1>
-          
         </section>
 
-        <section className="bg-white p-8 rounded-lg shadow-xl text-center" style={{ maxWidth: '700px', margin: '0 auto', border: '1px solid #e2e8f0', boxShadow: '0 10px 25px rgba(0,0,0,0.1)' }}>
+        <section className="bg-white mb-20 p-8 rounded-lg shadow-xl text-center" style={{ maxWidth: '700px', margin: '0 auto', border: '1px solid #e2e8f0', boxShadow: '0 10px 25px rgba(0,0,0,0.1)' }}>
           <FileUpload />
 
           <p style={{ fontSize: '20px', fontFamily: 'Helvetica Neue, sans-serif', color: '#333', lineHeight: '1.8', marginTop: '20px' }}>
-            <strong style={{ fontSize: '22px', color: '#2A2A2A' }}>CellSAM</strong> is a cutting-edge foundation model trained on a diverse range of cells and data types for cell segmentation.
-            Discover more in the <a href="https://www.biorxiv.org/content/10.1101/2023.11.17.567630v2.full.pdf" target="_blank" rel="noopener noreferrer" style={{ color: '#4A90E2', textDecoration: 'none', fontWeight: 'bold' }}>preprint </a>   
-             and try out CellSAM above! CellSAM is proudly hosted by <a href="https://brev.dev" target="_blank" rel="noopener noreferrer" style={{ color: '#4A90E2', textDecoration: 'none', fontWeight: 'bold' }}>Brev.dev</a> and is a part of the 
+            <strong style={{ fontSize: '22px', color: '#2A2A2A' }}>CellSAM</strong> is a foundation model for cell segmentation trained on a diverse range of cells and data types.
+            Discover more in the <a href="https://www.biorxiv.org/content/10.1101/2023.11.17.567630v2.full.pdf" target="_blank" rel="noopener noreferrer" style={{ color: '#4A90E2', textDecoration: 'none', fontWeight: 'bold' }}>preprint </a> 
+            and try out CellSAM above! CellSAM is proudly hosted by <a href="https://brev.dev" target="_blank" rel="noopener noreferrer" style={{ color: '#4A90E2', textDecoration: 'none', fontWeight: 'bold' }}>Brev.dev</a> and is a part of the 
             <a href="https://deepcell.org" target="_blank" rel="noopener noreferrer" style={{ color: '#4A90E2', textDecoration: 'none', fontWeight: 'bold' }}> DeepCell ecosystem</a>.
           </p>
+        </section>
+
+        <section className="bg-white p-8 rounded-lg shadow-xl text-center" style={{ maxWidth: '700px', margin: '0 auto 40px auto', border: '1px solid #e2e8f0', boxShadow: '0 10px 25px rgba(0,0,0,0.1)' }}>
+          <h3 className="text-3xl font-bold text-gray-800 mb-4">Image Upload Guidelines</h3>
+          <DownloadButton />
+          <div className="mt-4 text-left" style={{ fontSize: '18px', fontFamily: 'Helvetica Neue, sans-serif', color: '#333', textIndent: '20px' }}>
+            <ul className="list-disc list-inside pl-6">
+              <li>Images should be in PNG format.</li>
+              <li>Acceptable channels: single channel, 2 channel (nuclear and whole cell), or 3 channel (blank, nuclear, and whole cell).</li>
+              <li>Other formats or channel configurations will throw an error.</li>
+            </ul>
+          </div>
         </section>
       </main>
     </div>
