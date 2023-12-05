3ade5cf8a4688ba165ebc9c6b3da622098ed54c2
Rohit Dilip <rdilip1137@gmail.com>
2023-12-04 21:49:24 -0800

    boudning boxes wokring p good

--- src/ImageCanvas.tsx
+++ src/ImageCanvas.tsx
@@ -14,27 +14,37 @@ export interface BoundingBox {
 
 const ImageCanvas: React.FC<ImageCanvasProps> = ({ imageSrc, onBoundingBoxesChange }) => {
   const canvasRef = useRef<HTMLCanvasElement>(null);
+  const imageRef = useRef(new Image());
   const [boundingBoxes, setBoundingBoxes] = useState<BoundingBox[]>([]);
   const [isDrawing, setIsDrawing] = useState(false);
   const [startPoint, setStartPoint] = useState({ x: 0, y: 0 });
 
   useEffect(() => {
     const canvas = canvasRef.current;
-    if (canvas) {
-      const context = canvas.getContext('2d');
-      const image = new Image();
+    const context = canvas?.getContext('2d');
+    const image = imageRef.current;
 
-      image.onload = () => {
-        if (context) {
-          canvas.width = image.width;
-          canvas.height = image.height;
-          context.drawImage(image, 0, 0);
-        }
-      };
+    image.onload = () => {
+      if (canvas && context) {
+        canvas.width = image.width;
+        canvas.height = image.height;
+        context.drawImage(image, 0, 0);
+        drawBoxes(context, boundingBoxes);
+      }
+    };
 
-      image.src = imageSrc;
-    }
-  }, [imageSrc]);
+    image.src = imageSrc;
+  }, [imageSrc, boundingBoxes]);
+
+  const drawBoxes = (context: CanvasRenderingContext2D, boxes: BoundingBox[]) => {
+    boxes.forEach(box => {
+      context.beginPath();
+      context.strokeStyle = 'red';
+      context.lineWidth = 2;
+      context.rect(box.startX, box.startY, box.endX - box.startX, box.endY - box.startY);
+      context.stroke();
+    });
+  };
 
   const handleMouseDown = (e: MouseEvent) => {
     const rect = canvasRef.current?.getBoundingClientRect();
@@ -69,11 +79,31 @@ const ImageCanvas: React.FC<ImageCanvasProps> = ({ imageSrc, onBoundingBoxesChan
     onBoundingBoxesChange(updatedBoxes); // Notify parent component
   };
 
-  const handleMouseMove = (e: MouseEvent) => {
-    if (!isDrawing) return;
-    // Drawing logic goes here
-    // ...
+  
+  const handleMouseMove = (e: MouseEvent<HTMLCanvasElement>) => {
+    if (!isDrawing || !canvasRef.current) return;
+    const rect = canvasRef.current.getBoundingClientRect();
+    const currentPoint = {
+      x: e.clientX - rect.left,
+      y: e.clientY - rect.top
+    };
+
+    const context = canvasRef.current.getContext('2d');
+    if (context) {
+      context.clearRect(0, 0, canvasRef.current.width, canvasRef.current.height);
+      context.drawImage(imageRef.current, 0, 0); // Use imageRef.current here
+      drawBoxes(context, boundingBoxes); // Redraw existing boxes
+
+      // Draw the current box
+      context.beginPath();
+      context.strokeStyle = 'red';
+      context.lineWidth = 2;
+      context.rect(startPoint.x, startPoint.y, currentPoint.x - startPoint.x, currentPoint.y - startPoint.y);
+      context.stroke();
+    }
   };
+
+
 
   return (
     <canvas
@@ -81,6 +111,7 @@ const ImageCanvas: React.FC<ImageCanvasProps> = ({ imageSrc, onBoundingBoxesChan
       onMouseDown={handleMouseDown}
       onMouseUp={handleMouseUp}
       onMouseMove={handleMouseMove}
+      style={{ border: '1px solid black' }} // Optional: added for visibility
     />
   );
 };
