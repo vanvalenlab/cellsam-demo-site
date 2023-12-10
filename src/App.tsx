80a046ce5ece41c544b716c296b37619624aa13e
Rohit Dilip <rdilip1137@gmail.com>
2023-12-10 01:18:05 -0800

    get rid of file uploader

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import FileUpload from "./FileUpload";
 import Header from "./Header";
 import DownloadButton from "./Download";
 import Gallery from "./Gallery";
-import ImageUploader from "./ImageUploader";
 
 function App() {
   return (
